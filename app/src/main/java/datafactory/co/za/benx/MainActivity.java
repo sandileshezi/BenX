275a0f26f92c5fbea319d79867a2985a3255163e
MAXXOR\sandile.shezi <sandile.shezi@maxxor.com>
2017-08-25 15:07:26 +0200

    landing activity design

--- app/src/main/java/datafactory/co/za/benx/MainActivity.java
+++ app/src/main/java/datafactory/co/za/benx/MainActivity.java
@@ -2,15 +2,18 @@ package datafactory.co.za.benx;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by sandile.shezi on 2017/08/25.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
